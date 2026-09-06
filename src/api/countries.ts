2cd6e06d1cce438a4b8134b46703870a0ac7a7ec
Babatunde Lamidi <odunayohlamidi@gmail.com>
2026-09-06 21:49:03 +0100

    Update BASE_URL for countries API endpoint

--- src/api/countries.ts
+++ src/api/countries.ts
@@ -1,6 +1,6 @@
 import type { Country } from '../types/country';
 
-const BASE_URL = 'https://restcountries.com/v3.1';
+const BASE_URL = 'https://api.restcountries.com/countries/v5';
 
 export const fetchAllCountries = async (): Promise<Country[]> => {
     const response = await fetch(`${BASE_URL}/all?fields=name,flags,cca3,region,subregion,population`);
