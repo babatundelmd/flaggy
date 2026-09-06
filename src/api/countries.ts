7639607f2e1a2aa686d9ea52b75814226931a46e
Babatunde Lamidi <odunayohlamidi@gmail.com>
2026-09-06 22:32:10 +0100

    Update fetchAllCountries return type to any[]

--- src/api/countries.ts
+++ src/api/countries.ts
@@ -2,7 +2,7 @@
 
 const BASE_URL = 'https://api.restcountries.com/countries/v5';
 
-export const fetchAllCountries = async (): Promise<any> => {
+export const fetchAllCountries = async (): Promise<any[]> => {
     const response = await fetch(`${BASE_URL}`,
                                  { headers: { 'Authorization': 'Bearer rc_live_a11e5bd9187f4500bf1608adb79ac6e0' } });
     if (!response.ok) {
