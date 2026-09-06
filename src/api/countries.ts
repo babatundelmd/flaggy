c2fce714b0bf5ae5c4c39568f23a33981f54a4a9
Babatunde Lamidi <odunayohlamidi@gmail.com>
2026-09-06 22:24:21 +0100

    refactor api call

--- src/api/countries.ts
+++ src/api/countries.ts
@@ -2,8 +2,8 @@ import type { Country } from '../types/country';
 
 const BASE_URL = 'https://api.restcountries.com/countries/v5';
 
-export const fetchAllCountries = async (): Promise<Country[]> => {
-    const response = await fetch(`${BASE_URL}/all?fields=name,flags,cca3,region,subregion,population`,
+export const fetchAllCountries = async (): Promise<any[]> => {
+    const response = await fetch(`${BASE_URL}`,
                                  { headers: { 'Authorization': 'Bearer rc_live_a11e5bd9187f4500bf1608adb79ac6e0' } });
     if (!response.ok) {
         throw new Error('Failed to fetch countries');
